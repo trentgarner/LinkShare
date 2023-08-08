25a59424df8b7d36e96a97484f513d0c03b9c23d
Trent Garner <trentongarner.13@icloud.com>
2023-08-08 15:57:17 -0600

    Add Links MVC

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Link.create({title: 'My First Link', url: "google.com"})
